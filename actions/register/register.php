6d33ea9f45350ba91abf0224029aad40851de643
Miguel Borges <up202004481@edu.fe.up.pt>
2023-11-30 18:55:39 +0000

    epic win 420 noscope

--- actions/register/register.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $email = $_POST['user_email'];
-    $pass = $_POST['password'];
-    $confirm = $_POST['confirm'];
-    if ($confirm != $pass)
-            header('Location:/register/index.html')
-    
-?>
