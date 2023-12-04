d574a185bba2c591062998ea24586b85f4154966
Miguel Borges <up202004481@edu.fe.up.pt>
2023-12-04 23:56:47 +0000

    login working

--- actions/login/index.php
+++ actions/login/index.php
@@ -1,10 +1,27 @@
 <?php
 	session_start();
-	$email = $_POST['user_email'];
-	$pass = $_POST['password'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
-	# TODO(Write the actual code)
-	$_SESSION['msg'] = "User not found";
-	header('Location:/login/');
+	$dbh = new PDO('sqlite:../../db');
+
+	$sq = $dbh->prepare('SELECT * FROM Student WHERE username=?;');
+	$sq->execute([$username]);
+	$user_exists = $sq->fetch();
+	if (!$user_exists) {
+		$_SESSION['msg'] = "User not found";
+		header('Location:/login/');
+		die();
+	}
+
+	$is_correct = password_verify($password, $user_exists['password_hash']);
+	if (!$is_correct) {
+		$_SESSION['msg'] = "Incorrect password";
+		header('Location:/login/');
+		die();
+	}
+
+	$_SESSION['user_id'] = $user_exists['id'];
+	header('Location:/');
 	die();
 ?>
