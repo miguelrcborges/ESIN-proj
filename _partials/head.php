b1630682cc831fb2d173dc58e7303384c7736891
Miguel Borges <up202004481@edu.fe.up.pt>
2023-12-05 09:35:29 +0000

    minimal fixes

--- _partials/head.php
+++ _partials/head.php
@@ -8,7 +8,7 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-	<title>Web Jonas<?php if ($title) { echo " - $title"; } ?></title>
+	<title>Web Jonas<?php if (isset($title)) { echo " - $title"; } ?></title>
 	<link rel="stylesheet" href="/css/global.css">
 	<?php if ($css) { ?><link rel="stylesheet" href="/css/<?php echo "$css"; ?>.css"><?php } ?>
 	<meta name="viewport" content="initial-scale=1.0, width=device-width" />
