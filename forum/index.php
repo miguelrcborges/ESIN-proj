133345ad7b67ea8414e50468fa1b10d20e4370b1
Miguel Borges <up202004481@edu.fe.up.pt>
2023-12-05 11:56:12 +0000

    small changes

--- forum/index.php
+++ forum/index.php
@@ -1,6 +1,8 @@
 <?php 
-	$css = ["index", "header"];
 	include_once("../_partials/must_login.php");
+
+	$title = "Login";
+	$css = ["index", "header"];
 	include_once("../_partials/head.php");
 	include_once("../_partials/header.php");
 ?>
