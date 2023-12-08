502cb7579fead8f821d9124589ec16f9fc1fb119
Miguel Borges <up202004481@edu.fe.up.pt>
2023-12-08 15:25:44 +0000

    proper mobile navigation

--- _partials/header.php
+++ _partials/header.php
@@ -1,3 +1,11 @@
+<?php 
+	if (isset($_SESSION['theme']) && $_SESSION['theme'] == 'light') {
+		$theme = "Dark Theme"; 
+	} else {
+		$theme = "Light Theme";
+	}
+?>
+
 <body>
 	<header>
 		<a href="/forum/"><img src="/assets/webjonas.png"></a>
@@ -5,17 +13,30 @@
 			<?php if ($user_is_admin) { echo "<a href='/admin/'><li>Admin Panel</li></a>"; } ?>
 			<a href="/forum/"><li>Forum</li></a>
 			<a href="/exercises/"><li>Exercises</li></a>
-			<a href="/actions/change_theme/"><li>
-<?php if (isset($_SESSION['theme']) && $_SESSION['theme'] == 'light') {
-	echo "Dark Theme"; 
-} else {
-	echo "Light Theme";
-}?>
-			</li></a>
+			<a href="/actions/change_theme/"><li><?php echo $theme ?></li></a>
 		</ul>
-		<a id="menu" href="/links/">
-			<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path d="M4 6H20M4 12H20M4 18H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-			</svg>
-		</a>
+			<label id="menu" for="nav">
+				<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M4 6H20M4 12H20M4 18H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+				</svg>
+			</label>
 	</header>
+	<input type="checkbox" id="nav" style="display: none">
+	<nav>
+		<label id="close-menu" for="nav">
+			<svg viewBox="-2.5 -2.5 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+				<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+				<g id="SVGRepo_iconCarrier">
+					<path d="M3 21.32L21 3.32001" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+					<path d="M3 3.32001L21 21.32" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+				</g>
+			</svg>
+		</label>
+		<ul>
+			<?php if ($user_is_admin) { echo "<a href='/admin/'><li>Admin Panel</li></a>"; } ?>
+			<a href="/forum/"><li>Forum</li></a>
+			<a href="/exercises/"><li>Exercises</li></a>
+			<a href="/actions/change_theme/"><li><?php echo $theme ?></li></a>
+		</ul>
+	</nav>
