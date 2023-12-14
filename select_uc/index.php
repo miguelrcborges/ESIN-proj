b839049f77ae905a085b370051cd113243356438
tomasccpt <tomascurralo2014@gmail.com>
2023-12-14 18:20:17 +0000

    Flexbox in select_uc Substituí a grid da select_uc por uma flexbox, testar a implementação com base de dados.

--- select_uc/index.php
+++ select_uc/index.php
@@ -16,22 +16,20 @@
 	$ucs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<body>
-	<main>
-		<h1 class="title">Choose what you will study today!</h1>
-		<?php if (isset($msg)) { ?>
-		<div class="message"><?php echo $msg ?></div>
-		<?php } ?>
-		<form action="/solve_exercise/" method='GET'>
-			<select name="uc_id">
-				<?php foreach($ucs as $uc){
-					echo '<option value="' . $uc['id'] . '" name="uc">' . $uc['name'] . '</option>';
-				} ?>
-			</select>
-			<button type="submit">Study</button>
-		</form>
-	</main>
-</body>
+<main>
+	<h1 class="title">Choose what you will study today!</h1>
+	<?php if (isset($msg)) { ?>
+	<div class="message"><?php echo $msg ?></div>
+	<?php } ?>
+	<form action="/solve_exercise/" method='GET'>
+		<select name="uc_id">
+			<?php foreach($ucs as $uc){
+				echo '<option value="' . $uc['id'] . '" name="uc">' . $uc['name'] . '</option>';
+			} ?>
+		</select>
+		<button type="submit">Study</button>
+	</form>
+</main>
 
 <?php
 	include_once("../_partials/footer.php");
