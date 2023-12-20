bee479df2a3eadd99f9b2cd28c743105bb65a4c1
FranciscoCamoesM <francisco.camoes.magalhaes@gmail.com>
2023-12-20 17:47:15 +0000

    Removi cenas estupidas trocadas por cascade. Fixed Display course error.

--- user_settings/change_username/index.php
+++ user_settings/change_username/index.php
@@ -2,7 +2,7 @@
 $page_title = "Change Username";
 include_once($_SERVER['DOCUMENT_ROOT'] . "/user_settings/_partials/head.php"); ?>
 
-<form action='\actions\user_settings\change_username' method='POST'>
+<form action='/actions/user_settings/change_username/' method='POST'>
     <p><strong>Current Username: </strong><?php echo $user_info["username"]?></p>
     <label for new_name><strong>New Username:</strong></label>
     <input type="text" name="new_name" placeholder="BabyGronk" required="required">
