c24d674876ba3519b01f805e1dba54e86ce5b42e
Miguel Borges <up202004481@edu.fe.up.pt>
2023-12-20 18:22:25 +0000

    le pull

--- _partials/header.php
+++ _partials/header.php
@@ -28,7 +28,7 @@
 			</svg>
 		</label>
 		<ul>
-			<?php if ($user_is_admin) { echo "<a href='/admin/'><li>Admin Panel</li></a>"; } ?>
+			<?php if ($user_is_admin) { echo "<a href='/admin_panel/'><li>Admin Panel</li></a>"; } ?>
 			<a href="/forum/"><li>Forum</li></a>
 			<a href="/select_uc/"><li>Exercises</li></a>
 			<a href="/actions/change_theme/"><li><?php echo $change_theme_label ?></li></a>
